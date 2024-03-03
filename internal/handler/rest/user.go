8d0ee617f76a02ac355b9e69721693d30890eff4
Syahreza-Ferdian <syahrezafistiferdian32@gmail.com>
2024-03-03 12:21:04 +0700

    `add`: message on duplicate email

--- internal/handler/rest/user.go
+++ internal/handler/rest/user.go
@@ -2,6 +2,7 @@ package rest
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/Syahreza-Ferdian/heal-in/model"
 	"github.com/Syahreza-Ferdian/heal-in/pkg/web/response"
@@ -20,9 +21,13 @@ func (r *Rest) CreateUser(c *gin.Context) {
 	// user service call
 	err = r.service.UserService.Register(userRequest)
 	if err != nil {
+		if strings.Contains(err.Error(), "Error 1062") {
+			response.OnFailed(c, http.StatusConflict, "Email sudah terdaftar", err)
+			return
+		}
 		response.OnFailed(c, http.StatusInternalServerError, "Gagal registrasi user", err)
 		return
 	}
 
-	response.OnSuccess(c, http.StatusCreated, "Berhasil registrasi user", "Data is secret, you can not see it!")
+	response.OnSuccess(c, http.StatusCreated, "Berhasil registrasi user", nil)
 }
