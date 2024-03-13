729441cb4180bf284d54c18229a77f65b4ddc0ae
Syahreza-Ferdian <syahrezafistiferdian32@gmail.com>
2024-03-13 17:43:00 +0700

    `refractor`: fix email sender

--- pkg/email/email.go
+++ pkg/email/email.go
@@ -1,11 +1,9 @@
 package email
 
 import (
-	"bytes"
 	"crypto/tls"
-	"html/template"
+	"fmt"
 	"os"
-	"path/filepath"
 	"strconv"
 
 	"github.com/Syahreza-Ferdian/heal-in/model"
@@ -19,7 +17,6 @@ type EmailService interface {
 
 type EmailSender struct {
 	Name      string
-	FromEmail string
 	Password  string
 }
 
@@ -30,10 +27,9 @@ type EmailData struct {
 	WebURL      string
 }
 
-func NewEmailSender(name, fromEmail, password string) EmailService {
+func NewEmailSender(name, password string) EmailService {
 	return &EmailSender{
 		Name:      name,
-		FromEmail: fromEmail,
 		Password:  password,
 	}
 }
@@ -41,51 +37,22 @@ func NewEmailSender(name, fromEmail, password string) EmailService {
 func (e *EmailSender) SendEmail(user *model.UserRegister, data *EmailData) error {
 	serverPort, _ := strconv.Atoi(os.Getenv("SMTP_PORT"))
 
-	// parse template
-	tmpl, err := ParseTemplateDir("../../pkg/email/template")
-	if err != nil {
-		panic(err)
-	}
-
-	// create a new buffer
-	var body bytes.Buffer
-
-	// execute the template and write it to the buffer
-	tmpl.ExecuteTemplate(&body, "base.html", &data)
+	body := fmt.Sprintf("<p>Hi, %s</p> <p>Follow this link below to confirm your email address. If you didn't create an account with <a href='%s'>Heal.in</a>, you can safely delete this email.</p> <a href='%s'>Klik</a> <br> <p>You received this email because we received a request for registration for your account. If you didn't request that registration into our service, you can safely delete this email.</p>", data.FirstName, data.WebURL, data.RedirectURL)
 
 	m := gomail.NewMessage()
-	m.SetHeader("From", e.FromEmail)
+	m.SetHeader("From", e.Name)
 	m.SetHeader("To", user.Email)
 	m.SetHeader("Subject", data.Subject)
-	m.SetBody("text/html", body.String())
-	m.AddAlternative("text/plain", html2text.HTML2Text(body.String()))
+	m.SetBody("text/html", body)
+	m.AddAlternative("text/plain", html2text.HTML2Text(body))
 
 	dialer := gomail.NewDialer(os.Getenv("SMTP_HOST"), serverPort, e.Name, e.Password)
 	dialer.TLSConfig = &tls.Config{InsecureSkipVerify: true}
 
-	err = dialer.DialAndSend(m)
+	err := dialer.DialAndSend(m)
 	if err != nil {
 		return err
 	}
 
 	return nil
-}
-
-func ParseTemplateDir(dir string) (*template.Template, error) {
-	var paths []string
-	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		if !info.IsDir() {
-			paths = append(paths, path)
-		}
-		return nil
-	})
-
-	if err != nil {
-		return nil, err
-	}
-
-	return template.ParseFiles(paths...)
 }
