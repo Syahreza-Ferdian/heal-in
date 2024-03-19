a4550e88d68f84d9a9f691d5f5676834f93109a3
Syahreza-Ferdian <syahrezafistiferdian32@gmail.com>
2024-03-19 23:41:40 +0700

    `add`: custom field parameter on payment request

--- internal/service/payment.go
+++ internal/service/payment.go
@@ -19,7 +19,7 @@ import (
 
 type InterfacePaymentService interface {
 	NewPayment(paymentReq model.MidtransRequest, c *gin.Context) (model.MidtransResponse, error)
-	PaymentNotification(payload model.NotificationPayload) error
+	// PaymentNotification(payload model.NotificationPayload) error
 	SendEmailToExpiredSubs() ([]email.EmailDataExpSubs, error)
 	SetupScheduler()
 }
@@ -77,6 +77,7 @@ func (ps *PaymentService) NewPayment(paymentReq model.MidtransRequest, c *gin.Co
 				Name:  paymentReq.Description,
 			},
 		},
+		CustomField1: "subscription payment",
 	}
 
 	response, err := ps.snapClient.CreateTransaction(&snapReq)
@@ -102,30 +103,30 @@ func (ps *PaymentService) NewPayment(paymentReq model.MidtransRequest, c *gin.Co
 	return midtransResponse, nil
 }
 
-func (ps *PaymentService) PaymentNotification(payload model.NotificationPayload) error {
-	orderID, oke := payload["order_id"].(string)
+// func (ps *PaymentService) PaymentNotification(payload model.NotificationPayload) error {
+// 	orderID, oke := payload["order_id"].(string)
 
-	if !oke {
-		return fmt.Errorf("failed to get order_id")
-	}
+// 	if !oke {
+// 		return fmt.Errorf("failed to get order_id")
+// 	}
 
-	transactionResponse, err := ps.coreApi.CheckTransaction(orderID)
+// 	transactionResponse, err := ps.coreApi.CheckTransaction(orderID)
 
-	if err != nil {
-		return err
-	}
+// 	if err != nil {
+// 		return err
+// 	}
 
-	if transactionResponse != nil {
-		if transactionResponse.TransactionStatus == "settlement" {
-			err := ps.pr.UpdatePaymentOnSuccess(orderID)
-			if err != nil {
-				return err
-			}
-		}
-	}
+// 	if transactionResponse != nil {
+// 		if transactionResponse.TransactionStatus == "settlement" {
+// 			err := ps.pr.UpdatePaymentOnSuccess(orderID)
+// 			if err != nil {
+// 				return err
+// 			}
+// 		}
+// 	}
 
-	return nil
-}
+// 	return nil
+// }
 
 func (ps *PaymentService) SendEmailToExpiredSubs() ([]email.EmailDataExpSubs, error) {
 	expPayments, err := ps.pr.GetExpiredSubscriptions()
@@ -169,9 +170,9 @@ func (ps *PaymentService) SetupScheduler() {
 	ps.scheduler.AddFunction("0 0 0 * * *", ps.ExpEmailSender)
 
 	// Testing buat memastikan scheduler jalan dengan baik
-	ps.scheduler.AddFunction("@every 5m", func() {
-		log.Println("Scheduler: If you seen this message, it means the scheduler is running properly.")
-	})
+	// ps.scheduler.AddFunction("@every 5m", func() {
+	// 	log.Println("Scheduler: If you seen this message, it means the scheduler is running properly.")
+	// })
 
 	ps.scheduler.Start()
 }
