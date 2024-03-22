411c95080ce41d42093b675477e78c5dcc2a1130
Syahreza-Ferdian <syahrezafistiferdian32@gmail.com>
2024-03-22 14:00:00 +0700

    `refractor`: change description coloumn data type to `text`

--- entity/podcast.go
+++ entity/podcast.go
@@ -10,7 +10,7 @@ type Podcast struct {
 	ID          uuid.UUID `json:"id" gorm:"type:varchar(36);primary_key;"`
 	Title       string    `json:"title" gorm:"type:varchar(255);not null;"`
 	Link        string    `json:"link" gorm:"type:varchar(255);not null;"`
-	Description string    `json:"description" gorm:"type:varchar(255)"`
+	Description string    `json:"description" gorm:"type:text"`
 	CreatedAt   time.Time `json:"created_at" gorm:"autoCreateTime"`
 	Thumbnail   string    `json:"thumbnail" gorm:"type:varchar(255)"`
 }
--- entity/video.go
+++ entity/video.go
@@ -10,7 +10,7 @@ type Video struct {
 	ID          uuid.UUID `json:"id" gorm:"type:varchar(36);primary_key;"`
 	Title       string    `json:"title" gorm:"type:varchar(255);not null;"`
 	Link        string    `json:"link" gorm:"type:varchar(255);not null;"`
-	Description string    `json:"description" gorm:"type:varchar(255)"`
+	Description string    `json:"description" gorm:"type:text"`
 	CreatedAt   time.Time `json:"created_at" gorm:"autoCreateTime"`
 	UpdatedAt   time.Time `json:"updated_at" gorm:"autoUpdateTime"`
 }
